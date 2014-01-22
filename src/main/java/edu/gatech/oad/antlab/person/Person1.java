77c28677f0d8a714c11db2eeb1424bb82debc40b
jrossi6 <jrossi6@gatech.edu>
2014-01-21 20:48:12 -0500

    Revert "Person1 method completed"
    This reverts commit bbc81c8db6d6f32d95f39ef627993c9279504662.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,11 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-      String output = input;
-      if (input.length() > 2) {
-        output = input.substring(2, input.length()-1) + input.substring(0,1);
-      }
-      return output;
+	  return null;
 	}
 	
 	/**
